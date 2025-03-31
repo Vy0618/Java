dfcbfd8a27d6a41ee4f2378edba30025ac7fbe45
Vy0618 <viniciusazevedo1a@gmail.com>
2025-03-31 15:05:33 -0300

    31/03 - Soma e média das idades

---
+++ somaemediaidades.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class somaemediaidades {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int idade, maior = 0, soma = 0;
+        double media = 0;
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("A " + i + "° idade");
+            idade = ler.nextInt();
+            soma += idade;
+            media = soma/20;
+            if (idade >= 18) {
+                maior++;
+            }
+        }
+
+
+        System.out.println("A soma entre todos as idades é equivalente a: " + soma + "\nA média das idades é igual a " + media + " \nA quantidade de pessoas maior de idade é igual a " + maior + ".");
+    }
+}
