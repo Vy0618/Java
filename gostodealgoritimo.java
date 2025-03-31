b5cf53ad34acf9fdb0a7142b84051ee1ff14f2e4
Vy0618 <viniciusazevedo1a@gmail.com>
2025-03-31 13:54:07 -0300

    Atividade - GOSTODEALGORITIMO 31/03

---
+++ gostodealgoritimo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class gostodealgoritimo {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int n;
+
+        System.out.println("DIGITE O QUANTO VOCÊ GOSTA DE ALGORITIMO!!!");
+        n = ler.nextInt();
+
+        for (int i = 0; i <= n; i++)
+            System.out.println("EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO EU GOSTO DE ALGORITIMO");
+
+    }
+}
