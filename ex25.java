238ba4a2357c560f4415c49d3c2ae35ae82c01d5
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-07 12:01:49 -0300

    07/04 - Ex25

---
+++ ex25.java
@@ -0,0 +1,13 @@
+public class ex25
+{
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num < 100){
+            num++;
+
+            System.out.println(+num);
+
+        }
+    }
+}
