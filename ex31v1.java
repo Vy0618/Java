67b05716b92b1e9c7a01bc3b64ba59da012a6714
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-07 12:09:31 -0300

    07/04 - Ex 31v1

---
+++ ex31v1.java
@@ -0,0 +1,12 @@
+public class ex31v1
+{
+    public static void main(String[] args) {
+        int numero = 0;
+        while(numero <= 19){
+            numero++;
+            System.out.println("numeros de 1 a 20: " +numero);
+
+        }
+
+    }
+}
