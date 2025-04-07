be1cef9b49ac325bbd3e8daee8b8383266b793ca
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-07 12:10:54 -0300

    07/04 - Ex31v2

---
+++ ex31v2.java
@@ -0,0 +1,12 @@
+public class ex31v2
+{
+    public static void main(String[] args) {
+        int numero = 0;
+        while(numero <= 19){
+            numero++;
+            System.out.print (numero+ " ");
+
+        }
+
+    }
+}
