32df79d129e7ffb4664fd80aba5d2e912e6bef98
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-09 10:23:36 -0300

    09/04 - Ex37 Cavalo

--- ex37.java
+++ ex37.java
@@ -1,19 +1,16 @@
-/******************************************************************************
- ex37
- Faça um algoritmo para calcular quantas ferraduras são necessárias para equipar todos os cavalos comprados para um haras.
-*******************************************************************************/
 import java.util.Scanner;
 
 public class ex37 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int cavalos, ferraduras;
+
         System.out.print("Digite o numero de cavalos no haras: ");
-        int horse = scanner.nextInt();
-        
-        int fncssary = horse * 4;
-        
-        System.out.println("O número total de ferraduras necessárias é: " + fncssary);
+        cavalos = scanner.nextInt();
+
+        ferraduras = cavalos * 4;
+
+        System.out.println("O número total de ferraduras necessárias é: " + cavalos);
     }
 }
