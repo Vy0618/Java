7199d3c4a1f80c0daafb39ec3dd18a37b5411943
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-09 07:58:19 -0300

    07/04 - Ex36 Banco

---
+++ ex36.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class ex36 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double dinheirobase, dinheirof, vvariavel;
+        int opcao = 0;
+
+
+        while(opcao != 4) {
+
+        dinheirobase = 10000;
+
+        System.out.println("Bom dia! Bem vindo ao nosso banco. Você deseja:\n1 - Verificar Saldo Bancário\n2 - Realizar depósito bancário\n3 - Sacar dinheiro\n4 - Sair.");
+        opcao = ler.nextInt();
+
+
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("\nSeu saldo bancário, no momento, é de R$" + dinheirobase + ".");
+                    break;
+
+                case 2:
+                    System.out.println("Qual é o valor que deseja depositar?");
+                    vvariavel = ler.nextDouble();
+
+                        dinheirof = dinheirobase + vvariavel;
+                        dinheirobase = dinheirof ;
+                        System.out.println(dinheirobase);
+                        break;
+
+                case 3:
+                    System.out.println("Qual é o valor que desejas sacar?");
+                    vvariavel = ler.nextDouble();
+
+                        dinheirof = dinheirobase - vvariavel;
+                        dinheirobase = dinheirof;
+                        break;
+
+                case 4:
+                    System.out.println("Foi um prazer trabalhar convosco! Volte sempre.");
+                    break;
+
+            }
+        }
+    }
+}
