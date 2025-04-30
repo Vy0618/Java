90843bc817e4c95e1522860529450b72c2aac5d2
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-30 09:57:09 -0300

    30/04 - Ex17

---
+++ ex173004.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ex173004 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+int n = 0;
+
+        for (int i = 0; i <= 50; i++) {
+            n++;
+            if(n % 2 != 0){
+                System.out.println(n);
+            }
+
+
+        }
+    }
+}
