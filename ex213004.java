eefd12cfe7240ea45126babdf851bb69e4fdcd81
Vy0618 <viniciusazevedo1a@gmail.com>
2025-04-30 08:42:07 -0300

    30/04 - Ex21

---
+++ ex213004.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ex213004 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        int[] luizao = new int[5];
+         luizao[0] = 1;
+         luizao[1] = 2;
+         luizao[2] = 3;
+         luizao[3] = 4;
+         luizao[4] = 5;
+
+        for (int i = 0; i < luizao.length; i++ ){
+            System.out.println(luizao[i]);
+        }
+
+
+
+
+
+
+
+
+    }
+}
